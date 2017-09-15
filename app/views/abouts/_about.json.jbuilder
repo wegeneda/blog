3bfe918c057c6c6771d1ac9d617ad410a6065008
David <david-wegener@web.de>
2017-09-15 14:33:34 +0200

    added_about

---
+++ app/views/abouts/_about.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! about, :id, :title, :body, :created_at, :updated_at
+json.url about_url(about, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :abouts
   devise_for :admins
   mount Ckeditor::Engine => '/ckeditor'
   root to: "posts#index"
